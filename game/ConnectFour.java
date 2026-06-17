2024cba7b867670829ce22d716f9db9b8770d4ad
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2026-06-17 17:32:55 -0300

    estrutura

---
+++ game/ConnectFour.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ConnectFour{
+    Scanner scan = new Scanner(System.in);
+    
+
+    public ConnectFour() {
+        
+
+    }
+
+    // Inicializacao
+    // -----------------------------------------------------------------
+
+    // Aqui a tabela será populada com a letra B
+    public void popular() {
+        
+    }
+
+
+    // Métodos Principais
+    // -----------------------------------------------------------------
+
+    //Todos os requisitos funcionais
+
+    // Métodos Secundários
+    // -----------------------------------------------------------------
+
+    //Métodos que vão ajudar com os requisitos funcionais
+
+    // Main
+    // -----------------------------------------------------------------
+
+    public static void main(String[] args) {
+        new Questao3(5, 15);
+    }
+}
