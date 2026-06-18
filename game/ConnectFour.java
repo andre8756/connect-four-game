73ee025418f57876c89a47a75e545410c992885b
LorenzoMaiola <lorenzogiusepemaiola@gmail.com>
2026-06-17 21:22:17 -0300

    Add métodos de popular e imprimir matriz

--- game/ConnectFour.java
+++ game/ConnectFour.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
-public class ConnectFour{
+public class ConnectFour {
+    private final int linhas = 6;
+    private final int colunas = 7;
+    String[][] tabuleiro = new String[linhas][colunas];
     Scanner scan = new Scanner(System.in);
-    
 
     public ConnectFour() {
-        
-
+        popular();
+        imprimirTabuleiro();
     }
 
     // Inicializacao
@@ -14,23 +16,35 @@ public class ConnectFour{
 
     // Aqui a tabela será populada com a letra B
     public void popular() {
-        
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                tabuleiro[i][j] = "[B]";
+            }
+        }
     }
 
-
     // Métodos Principais
+    private void imprimirTabuleiro() {
+    for (int i = 0; i < tabuleiro.length; i++) {
+        for (int j = 0; j < tabuleiro[i].length; j++) {
+            System.out.print(tabuleiro[i][j]);
+        }
+        System.out.println();
+    }
+}
     // -----------------------------------------------------------------
 
-    //Todos os requisitos funcionais
+    // Todos os requisitos funcionais
 
     // Métodos Secundários
     // -----------------------------------------------------------------
 
-    //Métodos que vão ajudar com os requisitos funcionais
+    // Métodos que vão ajudar com os requisitos funcionais
 
     // Main
     // -----------------------------------------------------------------
 
     public static void main(String[] args) {
+        new ConnectFour();
     }
 }
