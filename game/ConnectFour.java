65d099bf64b6778c46819f3a3badc5c217f6781e
LorenzoMaiola <lorenzogiusepemaiola@gmail.com>
2026-07-01 12:41:24 -0300

    adicionado espaçamento entre colunas

--- game/ConnectFour.java
+++ game/ConnectFour.java
@@ -31,7 +31,7 @@ public class ConnectFour {
     private void imprimirTabuleiro(char[][] tabuleiro) {
         for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro[i].length; j++) {
-                System.out.print(tabuleiro[i][j]);
+            System.out.print(tabuleiro[i][j] + " ");
             }
             System.out.println();
         }
